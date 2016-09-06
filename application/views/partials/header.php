8b951b9c44320a7539f2636ccb20b63cbfa9068d
Gurpreet Singh <gurpreet@iowiki.com>
2016-09-06 23:23:38 +0530

    iowiki main commit 1

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -67,75 +67,65 @@
  
 <?php $categories = get_post_categories(); ?>	
 
-  <div class="header clearfix">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-2">
-                        <a href="/">
-                            <div class="logo">
-                                <div class='logo-title'>
-                                	IOWIKI 
-                                </div>
-                            </div>
-                        </a>    
-                    </div>
-                    <div class="col-xs-8">
-                        <nav class="navbar navbar-default">
-            			    <!-- Brand and toggle get grouped for better mobile display -->
-            			    <div class="navbar-header">
-            			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            			        <span class="sr-only">Toggle navigation</span>
-            			        <span class="icon-bar"></span>
-            			        <span class="icon-bar"></span>
-            			        <span class="icon-bar"></span>
-            			      </button>
-
-            			    </div>
-
-                      <? $controller = $this->router->fetch_class(); ?> 
-
-            			    <!-- Collect the nav links, forms, and other content for toggling -->
-
-            			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            			      <ul class="nav navbar-nav">
-                                <li class="logo-title"><a href="/"></a></li>
-            			        <li class="<?=($controller=='Home')?'active':''?>"><a href="/">Home <span class="sr-only">(current)</span></a></li>
-                             <?php foreach(get_pages_list() as $key => $value): ?>        
-            			        <li>
-                                    <a href="<?=site_url('pages/'.$value['page_slug'].'/'.$value['id'])?>">
-                                        <?=$value['page_name']?>
-                                    </a>
-                                </li>
-                            <?php endforeach; ?>    
-                            <!-- Categories Dropdown -->    
-                                <li class="dropdown  m-b<?=($controller=='category')?'active':''?>">
-                                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" data-submenu="" aria-expanded="false">Categories<span class="caret"></span></a>
-                                  <ul class="dropdown-menu">
-                                    <?php foreach ($categories as $key => $value): ?>
-                                         <li>
-                                            <a href="<?=site_url('category/'.seo_url($value['category_slug']))?>">
-                                                <?=$value['category_name']?>
-                                            </a>
-                                        </li>
-                                    <?  endforeach; ?>                                
-                                  </ul>
-                                </li>
-                            <!-- Network Dropdown ends -->    
-                                <li class="<?=($controller=='contact')?'active':''?>"><a href="<?=site_url('contact')?>">Contact</a></li>
-            			      </ul>
-            			      <form class="navbar-form navbar-left" method="get" action="<?=site_url('/search')?>">
-            			        <div class="form-group">
-            			          <input type="text" class="form-control" name="search_keyword" placeholder="Search">
-            			        </div>
-            			        <button type="submit" class="btn btn-default">Search</button>
-            			      </form>
-            			    </div><!-- /.navbar-collapse -->
-
-            		    </nav>
-
-                    </div>       <!-- col-xs-7 --> 
-                    <div class="col-md-2">
-                    	<div class="social-icons-header">
+<div class="header clearfix"
+<!-- New Navbar  -->
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-2">
+			 <img src="<?=base_url('assets/uploads/files/logo.png')?>" class="img-fit-containter" />
+		</div>
+		<div class="col-md-8">
+			<nav class="navbar navbar-default">
+			  <div class="container-fluid">
+			    <!-- Brand and toggle get grouped for better mobile display -->
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+			    </div>
+			    <? $controller = $this->router->fetch_class(); ?> 
+			    <!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			      <ul class="nav navbar-nav">
+			       <li class="<?=($controller=='Home')?'active':''?>"><a href="/">Home <span class="sr-only">(current)</span></a></li>
+			       <?php foreach(get_pages_list() as $key => $value): ?>        
+				        <li>
+			            <a href="<?=site_url('pages/'.$value['page_slug'].'/'.$value['id'])?>">
+			                <?=$value['page_name']?>
+			            </a>
+				        </li>
+			        <?php endforeach; ?>    
+			        <!-- Categories Dropdown -->    
+			            <li class="dropdown  m-b<?=($controller=='category')?'active':''?>">
+			              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" data-submenu="" aria-expanded="false">Categories<span class="caret"></span></a>
+			              <ul class="dropdown-menu">
+			                <?php foreach ($categories as $key => $value): ?>
+			                     <li>
+			                        <a href="<?=site_url('category/'.seo_url($value['category_slug']))?>">
+			                            <?=$value['category_name']?>
+			                        </a>
+			                    </li>
+			                <?  endforeach; ?>                                
+			              </ul>
+			            </li>
+			        <!-- Cateory Dropdown ends -->    
+			        <li class="<?=($controller=='contact')?'active':''?>"><a href="<?=site_url('contact')?>">Contact</a></li> 
+			      </ul>
+			      <form class="navbar-form navbar-left" method="get" action="<?=site_url('/search')?>">
+			        <div class="form-group">
+			          <input type="text" class="form-control" name="search_keyword" placeholder="Search">
+			        </div>
+			        <button type="submit" class="btn btn-default">Search</button>
+			      </form>
+			    </div><!-- /.navbar-collapse -->
+			  </div><!-- /.container-fluid -->
+			</nav>
+		</div> <!-- col-md-8 -->
+		<div class="col-md-2">
+				  	<div class="social-icons-header">
                     		<a target="_BLANK" href="https://www.facebook.com/iowiki/"><i class="icon-facebook"></i></a>
                         	<a target="_BLANK" href="https://www.twitter.com/iowiki"><i class="icon-twitter"></i></a>
                         	<a target="_BLANK" href=""><i class="icon-linkedin"></i></a>
@@ -143,15 +133,12 @@
                         	<a target="_BLANK" href="https://www.youtube.com/channel/UC4HgMfW9s_AdADMMeGDmAcg">
                         		<i class="icon-youtube"></i>
                         	</a>
-                        </div>	
-                    </div>
-
-                </div>        
-
-            </div>
-
-    </div> <!-- header -->
-
+                        </div>
+		</div>
+	</div>
+</div>
+<!-- New Navbar ends -->
+</div>
 <div class="container">  
 
   <div class="row">
